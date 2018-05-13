b98a845626d6ebe60e3e7752de45cbde16b8e073
RkEclair <ryosukadnak@gmail.com>
2018-05-13 18:01:09 +0900

    Modify bugs & update measure system

--- Akiha/Assets/Script/GameController.cs
+++ Akiha/Assets/Script/GameController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	PlayerController player;
+	Text signText;
+	Text currentTimeText;
+	Text recordText;
+	Text diffText;
+	Measurer currentMeasurer;
 
 	[SerializeField] GameObject[] stages;
-	Transform lastStageEnd = new Transform();
+	GameObject[] loadedStages = new GameObject[2];
+	Transform lastStageEnd;
 	int loadedIndex = 0;
 
 	// Use this for initialization
 	void Start() {
+		lastStageEnd = transform;
 		player = GameObject.Find("Player").GetComponent<PlayerController>();
+		signText = GameObject.Find("Sign").GetComponent<Text>();
+		currentTimeText = GameObject.Find("Timer").GetComponent<Text>();
+		recordText = GameObject.Find("Record").GetComponent<Text>();
+		diffText = GameObject.Find("Diff").GetComponent<Text>();
+
 		LoadStage();
 		LoadStage(); // Prefetching
 	}
@@ -24,13 +37,29 @@ public class GameController : MonoBehaviour {
 		if (stages[loadedIndex] == null)
 			return;
 
-		var newStage = Instantiate(stages[loadedIndex], lastStageEnd, 0,
+		if (loadedIndex > 2) {
+			Destroy(loadedStages[0]);
+		}
+
+		loadedStages[0] = loadedStages[1];
+
+		var newStage = Instantiate(stages[loadedIndex], lastStageEnd.position,
 			Quaternion.identity);
-		foreach (var child in newStage.transform.Find("")) {
+		foreach (Transform child in newStage.transform) {
 			if (child.tag == "Goal")
 				lastStageEnd = child;
+			else if (child.tag == "Finish") {
+				var prevMeasurer = currentMeasurer;
+				currentMeasurer = child.gameObject.GetComponent<Measurer>();
+				if (loadedIndex > 0) {
+					prevMeasurer.Init(currentTimeText, recordText, diffText);
+					prevMeasurer.MeasureStart();
+				}
+			}
 		}
-		loadedIndex++;
+		loadedStages[1] = newStage;
+		signText.text = "AREA: " + loadedIndex.ToString();
+		++loadedIndex;
 	}
 
 	public void Respawn() {
@@ -43,5 +72,10 @@ public class GameController : MonoBehaviour {
 
 	public void SetRespawn(Vector3 pos) {
 		player.SetRespawn(pos);
+	}
+
+	public void Goal(float score) {
+		LoadStage();
+		// TODO: save highscores
 	}
 }
--- Akiha/Assets/Script/Measurer.cs
+++ Akiha/Assets/Script/Measurer.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 public class Measurer : MonoBehaviour {
 	[SerializeField] GameObject door;
 	[SerializeField] bool visibleTime = true;
-	float time = 0.0f, startTime = 0.0f;
-	bool measuringTime = true;
+	float time = 0.0f, startTime = 0.0f, highscore = 0.0f;
+	bool measuringTime = false;
 
-	[SerializeField] Text currentTimeText;
-	[SerializeField] Text recordText;
+	Text currentTimeText;
+	Text recordText;
+	Text diffText;
 
 	GameController gameController;
 
@@ -19,40 +20,63 @@ public class Measurer : MonoBehaviour {
 	[SerializeField] AudioClip highscoreSound;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		source = gameObject.AddComponent<AudioSource>();
-		source.clip = highscoreSound;
+		source.clip = clearSound;
 		startTime = Time.time;
 		time = startTime;
-		currentTimeText = GameObject.Find("Timer").GetComponent<Text>();
-		recordText = GameObject.Find("Record").GetComponent<Text>();
 		gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
 	}
-	
+
+	public void Init(Text _current, Text _record, Text _diff) {
+		currentTimeText = _current;
+		recordText = _record;
+		diffText = _diff;
+	}
+
+	public void MeasureStart(float _highscore = 10000.0f) {
+		highscore = _highscore;
+		recordText.text = highscore.ToString("00.0000");
+		measuringTime = true;
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		if (measuringTime) {
 			time += Time.deltaTime;
-		}
-		if (visibleTime) {
-			currentTimeText.text = time.ToString("00.0000");
+			if (visibleTime) {
+				currentTimeText.text = time.ToString("00.0000");
+			}
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Player") {
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "Player" && measuringTime) {
 			measuringTime = false;
 			if (visibleTime) {
 				recordText.text = (time - startTime).ToString("00.0000");
 			}
 
-			gameController.LoadStage();
+			if (highscore == 10000.0f) {
+				diffText.text = "First record.";
+			}
+			else if ((time - startTime) < highscore) {
+				source.clip = highscoreSound;
+				diffText.text = "-" + (highscore - time + startTime).ToString("00.000");
+				diffText.color = Color.green;
+			}
+			else {
+				diffText.text = "+" + (time - startTime - highscore).ToString("00.000");
+				diffText.color = Color.red;
+			}
+
+			gameController.Goal(time - startTime);
 			source.Play();
 		}
 		door.SetActive(false);
 	}
 
-	void OnTriggerExit2D(Collider2D other) {
+	void OnCollisionExit2D(Collision2D col) {
 		door.SetActive(true);
 	}
 }
