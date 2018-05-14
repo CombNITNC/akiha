44fda406f95ed45cee5ceaa2347b0bc2700e4e92
RkEclair <ryosukadnak@gmail.com>
2018-05-15 02:36:34 +0900

    Add Mainmenu

---
+++ Akiha/Assets/Script/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	[SerializeField] bool clockwise = true;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		transform.Rotate(0f, Time.deltaTime * 10f * (clockwise ? 1f : -2f), 0f);
+	}
+}
