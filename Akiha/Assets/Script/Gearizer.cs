4119c754846c61d9e66382f928737cccdf50c6f2
RkEclair <ryosukadnak@gmail.com>
2018-05-18 21:18:46 +0900

    Modify typo

--- Akiha/Assets/Script/Gearizer.cs
+++ Akiha/Assets/Script/Gearizer.cs
@@ -9,7 +9,8 @@ public class Gearizer : MonoBehaviour {
   void Start() {
     Mesh mesh = GetComponent<MeshFilter>().mesh;
     Vector3[] vertexes = new Vector3[teeth * 6];
-    for (var i = 0, c = 0f, offset = 0f; i < teeth * 6; i += 6) {
+    float c = 0f, offset = 0f;
+    for (int i = 0; i < teeth * 6; i += 6) {
       if (i % 12 == 0)
         offset = 0.5f;
       else
@@ -17,8 +18,8 @@ public class Gearizer : MonoBehaviour {
 
       float x0 = Mathf.Cos(c * Mathf.Deg2Rad) * (2.5f + offset);
       float z0 = Mathf.Sin(c * Mathf.Deg2Rad) * (2.5f + offset);
-      float x0 = Mathf.Cos(c * Mathf.Deg2Rad) * 1.5f;
-      float z0 = Mathf.Sin(c * Mathf.Deg2Rad) * 1.5f;
+      float x0b = Mathf.Cos(c * Mathf.Deg2Rad) * 1.5f;
+      float z0b = Mathf.Sin(c * Mathf.Deg2Rad) * 1.5f;
       float x1 = Mathf.Cos((c + 360f / teeth) * Mathf.Deg2Rad) * (2.5f + offset);
       float z1 = Mathf.Sin((c + 360f / teeth) * Mathf.Deg2Rad) * (2.5f + offset);
       float x1b = Mathf.Cos((c + 360f / teeth) * Mathf.Deg2Rad) * 1.5f;
@@ -31,6 +32,7 @@ public class Gearizer : MonoBehaviour {
       vertexes[i + 5] = new Vector3(x1b, z1b, 0);
       c += 360f / teeth;
     }
+
     mesh.vertices = vertexes;
     // TODO: UV
     int[] triangles = new int[mesh.vertices.Length];
@@ -39,7 +41,7 @@ public class Gearizer : MonoBehaviour {
     }
     mesh.triangles = triangles;
     mesh.RecalculateNormals();
-    maesh.RecalculateBounds();
+    mesh.RecalculateBounds();
     GetComponent<MeshFilter>().sharedMesh = mesh;
   }
 }
