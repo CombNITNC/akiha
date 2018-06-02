75291765a9bf73dd4b60342d091d7c79641e2f33
RkEclair <ryosukadnak@gmail.com>
2018-06-02 19:54:55 +0900

    Start to fix MainMenu & add ArrayDuplicator

---
+++ Akiha/Assets/Script/ArrayDuplicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayDuplicator : MonoBehaviour {
+	[SerializeField] Vector3 count = Vector3.one;
+	[SerializeField] Vector3 padding;
+	[SerializeField] Vector3 rotateOffset;
+
+	void Start() {
+		for (int z = 0; z < count.z; ++z) {
+			for (int y = 0; y < count.y; ++y) {
+				for (int x = 0; x < count.x; ++x) {
+					var clone = Instantiate(gameObject, new Vector3(x * padding.x + transform.position.x, y * padding.y + transform.position.y, z * padding.z + transform.position.z), Quaternion.identity);
+					Destroy(clone.GetComponent<ArrayDuplicator>());
+					clone.transform.parent = transform.parent;
+					clone.transform.Rotate(x * rotateOffset);
+				}
+			}
+		}
+	}
+}
